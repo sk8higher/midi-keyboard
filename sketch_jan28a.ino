0bace0b6945904a2a819ff349d5c15782859d3be
Nelly Kim <sk8high3r@ya.ru>
2023-02-05 18:27:20 +0300

    remade button array init

--- sketch_jan28a.ino
+++ sketch_jan28a.ino
@@ -4,21 +4,12 @@
 // номера midi-нот. стандартом принят отсчет от ноты c3 (60), в некоторых контроллерах yamaha - c4
 // 48 - c2, 60 - c3, 72 - c4?
 int default_notes[] = { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71 };
+
+// Номер октавы для ограничения переключений
 int octave = 3;
 
 // Номера пинов клавиш (c c# d d# e f f# g g# a a# b)
-#define C_BTN_PIN 2
-#define CD_BTN_PIN 3
-#define D_BTN_PIN 4
-#define DD_BTN_PIN 5
-#define E_BTN_PIN 6
-#define F_BTN_PIN 7
-#define FD_BTN_PIN 8
-#define G_BTN_PIN 9
-#define GD_BTN_PIN 10
-#define A_BTN_PIN 11
-#define AD_BTN_PIN 12
-#define B_BTN_PIN 13
+const int buttonPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
 // Пин потенциометра (ручки громкости)
 #define POT_PIN A0
@@ -28,86 +19,23 @@ int octave = 3;
 #define MINUS_OCT_BTN_PIN A2
 
 // Создаем объекты кнопок для прослушивания ивентов на них
-Bounce2::Button cButton = Bounce2::Button();
-Bounce2::Button cdButton = Bounce2::Button();
-Bounce2::Button dButton = Bounce2::Button();
-Bounce2::Button ddButton = Bounce2::Button();
-Bounce2::Button eButton = Bounce2::Button();
-Bounce2::Button fButton = Bounce2::Button();
-Bounce2::Button fdButton = Bounce2::Button();
-Bounce2::Button gButton = Bounce2::Button();
-Bounce2::Button gdButton = Bounce2::Button();
-Bounce2::Button aButton = Bounce2::Button();
-Bounce2::Button adButton = Bounce2::Button();
-Bounce2::Button bButton = Bounce2::Button();
-
 Bounce2::Button plusOctButton = Bounce2::Button();
 Bounce2::Button minusOctButton = Bounce2::Button();
 
-// const byte buttonPins[] = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
-// const byte buttonCount = sizeof(buttonPins);
-// Bounce2::Button buttons[buttonCount];
+const int buttonCount = 12;
 
-// нужна для отсылки ивента noteoff
-byte last_played_note;
+Bounce2::Button buttons[buttonCount];
 
 // чета там инициализируем шоб миди работало
 USBMIDI_CREATE_DEFAULT_INSTANCE();
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  // Потенциометр будет вводить значения
   pinMode(POT_PIN, INPUT);
   // прилепляем обработчик событий на каждую кнопку
   // interval - debounce interval
   // pressedState - низкий т. к. не используется внешний резистор
-  cButton.attach(C_BTN_PIN, INPUT_PULLUP);
-  cButton.interval(25);
-  cButton.setPressedState(LOW);
-
-  cdButton.attach(CD_BTN_PIN, INPUT_PULLUP);
-  cdButton.interval(25);
-  cdButton.setPressedState(LOW);
-
-  dButton.attach(D_BTN_PIN, INPUT_PULLUP);
-  dButton.interval(25);
-  dButton.setPressedState(LOW);
-
-  ddButton.attach(DD_BTN_PIN, INPUT_PULLUP);
-  ddButton.interval(25);
-  ddButton.setPressedState(LOW);
-
-  eButton.attach(E_BTN_PIN, INPUT_PULLUP);
-  eButton.interval(25);
-  eButton.setPressedState(LOW);
-
-  fButton.attach(F_BTN_PIN, INPUT_PULLUP);
-  fButton.interval(25);
-  fButton.setPressedState(LOW);
-
-  fdButton.attach(FD_BTN_PIN, INPUT_PULLUP);
-  fdButton.interval(25);
-  fdButton.setPressedState(LOW);
-
-  gButton.attach(G_BTN_PIN, INPUT_PULLUP);
-  gButton.interval(25);
-  gButton.setPressedState(LOW);
-
-  gdButton.attach(GD_BTN_PIN, INPUT_PULLUP);
-  gdButton.interval(25);
-  gdButton.setPressedState(LOW);
-
-  aButton.attach(A_BTN_PIN, INPUT_PULLUP);
-  aButton.interval(25);
-  aButton.setPressedState(LOW);
-
-  adButton.attach(AD_BTN_PIN, INPUT_PULLUP);
-  adButton.interval(25);
-  adButton.setPressedState(LOW);
-
-  bButton.attach(B_BTN_PIN, INPUT_PULLUP);
-  bButton.interval(25);
-  bButton.setPressedState(LOW);
-
 
   plusOctButton.attach(PLUS_OCT_BTN_PIN, INPUT_PULLUP);
   plusOctButton.interval(25);
@@ -117,11 +45,11 @@ void setup() {
   minusOctButton.interval(25);
   minusOctButton.setPressedState(LOW);
 
-  // for (int i = 0; i < buttonCount; i++) {
-  //   buttons[i].attach(buttonPins[i], INPUT_PULLUP);
-  //   buttons[i].interval(25);
-  //   buttons[i].setPressedState(LOW);
-  // }
+  for (int i = 0; i < buttonCount; i++) {
+    buttons[i].attach(buttonPins[i], INPUT_PULLUP);
+    buttons[i].interval(25);
+    buttons[i].setPressedState(LOW);
+  }
 
   MIDI.begin(1);
   Serial.begin(9600);
@@ -131,19 +59,7 @@ void setup() {
 void loop() {
   int rotat, velo;
   // обновляем состояние кнопки
-  // for(int i = 0; i < buttonCount; i++) buttons[i].update();
-  cButton.update();
-  cdButton.update();
-  dButton.update();
-  ddButton.update();
-  eButton.update();
-  fButton.update();
-  fdButton.update();
-  gButton.update();
-  gdButton.update();
-  aButton.update();
-  adButton.update();
-  bButton.update();
+  for(int i = 0; i < buttonCount; i++) buttons[i].update();
 
   plusOctButton.update();
   minusOctButton.update();
@@ -171,87 +87,75 @@ void loop() {
     }
   }
 
-  if (cButton.pressed()) {
-    last_played_note = default_notes[0];
-
+  if (buttons[0].pressed()) {
     MIDI.sendNoteOn(default_notes[0], velo, 1);
-  } else if(cButton.released()) {
+  } else if(buttons[0].released()) {
     MIDI.sendNoteOff(default_notes[0], 0, 1);
   }
 
-  if (cdButton.pressed()) {
-    last_played_note = default_notes[1];
-
+  if (buttons[1].pressed()) {
     MIDI.sendNoteOn(default_notes[1], velo, 1);
-  } else if(cdButton.released()) {
+  } else if(buttons[1].released()) {
     MIDI.sendNoteOff(default_notes[1], 0, 1);
   }
 
-  if (dButton.pressed()) {
-    last_played_note = default_notes[2];
-
+  if (buttons[2].pressed()) {
     MIDI.sendNoteOn(default_notes[2], velo, 1);
-  } else if(dButton.released()) {
+  } else if(buttons[2].released()) {
     MIDI.sendNoteOff(default_notes[2], 0, 1);
   }
 
-  if (ddButton.pressed()) {
-    last_played_note = default_notes[3];
-
+  if (buttons[3].pressed()) {
     MIDI.sendNoteOn(default_notes[3], velo, 1);
-  } else if(ddButton.released()) {
+  } else if(buttons[3].released()) {
     MIDI.sendNoteOff(default_notes[3], 0, 1);
   }
 
-  if (eButton.pressed()) {
-    last_played_note = default_notes[4];
-
+  if (buttons[4].pressed()) {
     MIDI.sendNoteOn(default_notes[4], velo, 1);
-  } else if(eButton.released()) {
+  } else if(buttons[4].released()) {
     MIDI.sendNoteOff(default_notes[4], 0, 1);
   }
 
-  if (fButton.pressed()) {
-    last_played_note = default_notes[5];
-
+  if (buttons[5].pressed()) {
     MIDI.sendNoteOn(default_notes[5], velo, 1);
+  } else if(buttons[5].released()) {
+    MIDI.sendNoteOff(default_notes[5], 0, 1);
   }
 
-  if (fdButton.pressed()) {
-    last_played_note = default_notes[6];
-
+  if (buttons[6].pressed()) {
     MIDI.sendNoteOn(default_notes[6], velo, 1);
+  } else if(buttons[6].released()) {
+    MIDI.sendNoteOff(default_notes[6], 0, 1);
   }
 
-  if (gButton.pressed()) {
-    last_played_note = default_notes[7];
-
+  if (buttons[7].pressed()) {
     MIDI.sendNoteOn(default_notes[7], velo, 1);
+  } else if(buttons[7].released()) {
+    MIDI.sendNoteOff(default_notes[7], 0, 1);
   }
 
-  if (gdButton.pressed()) {
-    last_played_note = default_notes[8];
-
+  if (buttons[8].pressed()) {
     MIDI.sendNoteOn(default_notes[8], velo, 1);
+  } else if(buttons[8].released()) {
+    MIDI.sendNoteOff(default_notes[8], 0, 1);
   }
 
-  if (aButton.pressed()) {
-    last_played_note = default_notes[9];
-
+  if (buttons[9].pressed()) {
     MIDI.sendNoteOn(default_notes[9], velo, 1);
+  } else if(buttons[9].released()) {
+    MIDI.sendNoteOff(default_notes[9], 0, 1);
   }
 
-  if (adButton.pressed()) {
-    last_played_note = default_notes[10];
-
+  if (buttons[10].pressed()) {
     MIDI.sendNoteOn(default_notes[10], velo, 1);
+  } else if(buttons[10].released()) {
+    MIDI.sendNoteOff(default_notes[10], 0, 1);
   }
 
-  if (bButton.pressed()) {
-    last_played_note = default_notes[11];
-
+  if (buttons[11].pressed()) {
     MIDI.sendNoteOn(default_notes[11], velo, 1);
+  } else if(buttons[11].released()) {
+    MIDI.sendNoteOff(default_notes[11], 0, 1);
   }
-
-  //MIDI.sendNoteOff(last_played_note, 0, 1);
 }
